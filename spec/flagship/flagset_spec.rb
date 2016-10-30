b67b619cdf4745dd1239088bb18e2ff4924106df
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-10-31 03:25:51 +0900

    Implement extending to Flagset

--- spec/flagship/flagset_spec.rb
+++ spec/flagship/flagset_spec.rb
@@ -35,4 +35,56 @@ RSpec.describe Flagship::Flagset do
       end
     end
   end
+
+  describe 'extending' do
+    let(:base) do
+      described_class.new(:base, {
+        true_flag: true,
+        false_flag: false,
+        lambda_true_flag: -> { true },
+        lambda_false_flag: -> { false },
+      })
+    end
+
+    it 'extends base flagset' do
+      flagset = described_class.new(:extending, {}, base)
+
+      expect(flagset.enabled?(:true_flag)).to be true
+      expect(flagset.enabled?(:false_flag)).to be false
+      expect(flagset.enabled?(:lambda_true_flag)).to be true
+      expect(flagset.enabled?(:lambda_false_flag)).to be false
+    end
+
+    context 'with overriding flags' do
+      it 'changes flags' do
+        flagset = described_class.new(:extending, {
+          true_flag: false,
+          false_flag: true,
+          lambda_true_flag: -> { false },
+          lambda_false_flag: -> { true },
+        }, base)
+
+        expect(flagset.enabled?(:true_flag)).to be false
+        expect(flagset.enabled?(:false_flag)).to be true
+        expect(flagset.enabled?(:lambda_true_flag)).to be false
+        expect(flagset.enabled?(:lambda_false_flag)).to be true
+      end
+    end
+
+    context 'with new flags' do
+      it 'adds flags' do
+        flagset = described_class.new(:extending, {
+          new_true_flag: true,
+          new_false_flag: false,
+          new_lambda_true_flag: -> { true },
+          new_lambda_false_flag: -> { false },
+        }, base)
+
+        expect(flagset.enabled?(:new_true_flag)).to be true
+        expect(flagset.enabled?(:new_false_flag)).to be false
+        expect(flagset.enabled?(:new_lambda_true_flag)).to be true
+        expect(flagset.enabled?(:new_lambda_false_flag)).to be false
+      end
+    end
+  end
 end
