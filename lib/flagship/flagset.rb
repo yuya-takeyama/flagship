cbdbc933cb0bf58e14eff46d7f18f899ce5b5ada
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-11-23 23:13:32 +0900

    Implement tag extension

--- lib/flagship/flagset.rb
+++ lib/flagship/flagset.rb
@@ -6,7 +6,7 @@ class Flagship::Flagset
   def initialize(key, features_hash, base = nil)
     @key = key
     @features = base ?
-      base.features.map{ |f| [f.key, f] }.to_h.merge(features_hash) :
+      extend_features(features_hash, base) :
       features_hash
   end
 
@@ -19,5 +19,15 @@ class Flagship::Flagset
 
   def features
     @features.map { |key, feature| feature }
+  end
+
+  private
+
+  def extend_features(features_hash, base)
+    base.features.map { |f|
+      [f.key, f]
+    }.to_h.merge(features_hash) { |key, base_f, new_f|
+      new_f.extend_feature(base_f)
+    }
   end
 end
