aa6476f170475938e6e3b69c06cbc37964f7e208
Seth Jeffery <seth@quipper.com>
2016-12-22 11:33:10 +0000

    simplify

--- lib/flagship/features.rb
+++ lib/flagship/features.rb
@@ -1,17 +1,17 @@
 class Flagship::Features < Array
   def tagged(tags)
-    Flagship::Features.new(self.select{|feature| tags.any?{|tag, val| feature.tags[tag] == val}})
+    Flagship::Features.new(select{|feature| tags.any?{|tag, val| feature.tags[tag] == val}})
   end
 
   def tagged_all(tags)
-    Flagship::Features.new(self.select{|feature| tags.all?{|tag, val| feature.tags[tag] == val}})
+    Flagship::Features.new(select{|feature| tags.all?{|tag, val| feature.tags[tag] == val}})
   end
 
   def enabled
-    Flagship::Features.new(self.select{|feature| feature.enabled?})
+    Flagship::Features.new(select(&:enabled?))
   end
 
   def disabled
-    Flagship::Features.new(self.select{|feature| feature.disabled?})
+    Flagship::Features.new(select(&:disabled?))
   end
 end
